cd90ab6d16f61ee4fea93e9f2d89aa111bc6a9ee
bojorquezja <bojorquez.ja@gmail.com>
2026-05-05 13:56:20 -0500

    calculadora pos

---
+++ calcpos/PilaAr.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PilaArreglo {
+    internal class PilaAr {
+        //dato
+        private double[] datos = Array.Empty<double>();
+        
+        //inserta valor
+        public void Push(double valor) {
+            Array.Resize(ref datos, datos.Length+1);
+            datos[datos.Length - 1] = valor;
+        }
+        public double Pop() {
+            double valor = datos[datos.Length - 1];
+            Array.Resize(ref datos, datos.Length - 1);
+            return valor;
+        }
+        public double Top() {
+            return datos[datos.Length - 1];
+        }
+        public int Size() {
+            return datos.Length;
+        }
+        public bool Empty() {
+            return datos.Length == 0;
+        }
+        public override string? ToString() {
+            string str = "";
+            foreach (int i in datos) {
+                str += i.ToString() + ",";
+            }
+            return str;
+        }
+
+    }
+}
+/*
+
+
+
+
+*/
--- calcpos/Program.cs
+++ calcpos/Program.cs
@@ -4,3 +4,49 @@
 //si es una operacion, se quitan los 2 ultimo numneros de la pila,
 //se les opera y el resultado se devuelve a la pila. Ese resultado se mostrara en pantalla
 
+using PilaArreglo;
+
+PilaAr pila = new PilaAr();
+string entrada;
+
+Console.WriteLine("CALCULADORA POSTFIJA");
+Console.WriteLine("Ingrese números u operadores (+ - * /)");
+
+double res = 0;
+while (true) {
+    Console.Write("Entrada: ");
+    entrada = Console.ReadLine();
+
+    if (entrada.Trim() == "+") {
+        double b = pila.Pop();
+        double a = pila.Pop();
+        res = a + b;
+        pila.Push(res);
+        Console.WriteLine($"= {res}");
+    } else if (entrada.Trim() == "-") {
+        double b = pila.Pop();
+        double a = pila.Pop();
+        res = a - b;
+        pila.Push(res);
+        Console.WriteLine($"= {res}");
+    } else if (entrada.Trim() == "/") {
+        double b = pila.Pop();
+        double a = pila.Pop();
+        res = a / b;
+        pila.Push(res);
+        Console.WriteLine($"= {res}");
+    } else if (entrada.Trim() == "*") {
+        double b = pila.Pop();
+        double a = pila.Pop();
+        res = a * b;
+        pila.Push(res);
+        Console.WriteLine($"= {res}");
+
+        pila.Push(res);
+        Console.WriteLine($"Resultado: {res}");
+    } else if (double.TryParse(entrada, out double numero)) {
+            pila.Push(numero);
+    } else {
+        Console.WriteLine("Entrada no válida");
+    }
+}

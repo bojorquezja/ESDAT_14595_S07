c64532ceb3b23ba6f1dd9a85c5e731f9801e7f38
bojorquezja <bojorquez.ja@gmail.com>
2026-05-05 12:45:42 -0500

    Agregar archivos de proyecto.

---
+++ calcpos/Program.cs
@@ -0,0 +1,6 @@
+﻿// crear una calculadora post fija
+//debe pedirse al usuario que ingrese un numero o un signo de operacion: +, -, *, /
+//si es un numero se agrega a la pila
+//si es una operacion, se quitan los 2 ultimo numneros de la pila,
+//se les opera y el resultado se devuelve a la pila. Ese resultado se mostrara en pantalla
+
